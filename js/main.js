dd9877265b5be3af7ff8f3f00b314de9926e3d72
MohammedHassona99 <MohammedHassona99@hotmail.com>
2021-02-24 00:06:42 +0200

    feature section

--- js/main.js
+++ js/main.js
@@ -1,5 +1,19 @@
 $(function () {
+  // Trigger Nice Scroll Plugin
+
   $("html").niceScroll();
 
+  // Change Header Height
   $("header").height($(window).height());
+
+  // Scroll To Feature
+
+  $(".arrow i").click(function () {
+    $("html, body").animate(
+      {
+        scrollTop: $(".features").offset().top,
+      },
+      1000
+    );
+  });
 });
