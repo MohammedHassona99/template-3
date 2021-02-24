05e8bf947391829987c0e20d66f6382dfdae1e3e
MohammedHassona99 <MohammedHassona99@hotmail.com>
2021-02-24 23:56:39 +0200

    testimonial section

--- js/main.js
+++ js/main.js
@@ -22,4 +22,41 @@ $(function () {
   $(".show-more").click(function () {
     $(".our-work .hidden").fadeIn();
   });
+
+  //check testimonial
+  const arrowLeft = $(".arrow-left");
+  const arrowRight = $(".arrow-right");
+  function checkPerson() {
+    $(".person:first").hasClass("active")
+      ? arrowLeft.fadeOut()
+      : arrowLeft.fadeIn();
+
+    $(".person:last").hasClass("active")
+      ? arrowRight.fadeOut()
+      : arrowRight.fadeIn();
+  }
+
+  checkPerson();
+
+  $(".testimonial i").click(function () {
+    if ($(this).hasClass("arrow-right")) {
+      $(".testimonial .active").fadeOut(100, function () {
+        $(this)
+          .removeClass("active")
+          .next(".person")
+          .addClass("active")
+          .fadeIn();
+        checkPerson();
+      });
+    }else{
+            $(".testimonial .active").fadeOut(100, function () {
+              $(this)
+                .removeClass("active")
+                .prev(".person")
+                .addClass("active")
+                .fadeIn();
+              checkPerson();
+            });
+    }
+  });
 });
